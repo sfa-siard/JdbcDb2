f8890773eea527d920be8c981cb3b02364ab1613
Max Burri <burri@puzzle.ch>
2022-01-14 13:04:29 +0100

    build: create a testuser for the tests

--- docker/custom/01-initialize-db.sh
+++ docker/custom/01-initialize-db.sh
@@ -7,4 +7,5 @@ export PATH=/database/config/db2inst1/sqllib/bin/:$PATH
 su - db2inst1
 
 # connect to testdb (see docker-compose.yml) - username and password have to match
+# actually - we do nothing here for now. just keep it for documentation on how to setup a db2 with docker and docker compose
 db2 connect to TESTDB user db2inst1 using mypasswd

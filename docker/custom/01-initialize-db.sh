7700588f4c23ed2adb7616231d18f8d515f31b3a
Max Burri <burri@puzzle.ch>
2022-01-14 09:19:19 +0100

    build: adjust docker-compose.yml and wait for db to start

--- docker/custom/01-initialize-db.sh
+++ docker/custom/01-initialize-db.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "initialize custom db"
+# add db2 binaries to the path - for convinience reasons
 export PATH=/database/config/db2inst1/sqllib/bin/:$PATH
 
-db2 connect to TESTDB user db2inst1 using password
+# need to work as user db2inst1
+su - db2inst1
+
+# connect to testdb (see docker-compose.yml) - username and password have to match
+db2 connect to TESTDB user db2inst1 using mypasswd

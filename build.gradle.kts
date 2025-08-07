3bf8f58bfc3d3eed9204688857674bb5d6e57e87
Berivan Bertagna <bbertagna@puzzle.ch>
2025-08-07 12:03:42 +0200

    chore: upgrade to enterutilities:v2.2.4, SqlParser:v2.2.3, jdbc-base:v2.2.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 val versions = mapOf(
-    "jdbc-base" to "v2.2.9",
+    "jdbc-base" to "v2.2.10",
 )
 
 dependencies {
@@ -32,8 +32,8 @@ dependencies {
     implementation("org.antlr:antlr4-runtime:4.5.2")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.postgresql:postgresql:42.2.5")
-    implementation("ch.admin.bar:enterutilities:v2.2.3")
-    implementation("ch.admin.bar:SqlParser:v2.2.2")
+    implementation("ch.admin.bar:enterutilities:v2.2.4")
+    implementation("ch.admin.bar:SqlParser:v2.2.3")
     implementation("ch.admin.bar:jdbc-base:${versions["jdbc-base"]}")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
